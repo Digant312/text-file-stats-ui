fdc3ca7094bf6d69abe68fa26edcbccb3fe1fe55
Digant <digantpatel92@gmail.com>
2024-07-01 23:24:02 +0530

    error handling on api failure

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,6 +1,13 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import axios from "axios";
-import { Container, Typography, Box } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  Snackbar,
+  IconButton,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import StatsForm from "../components/StatsForm";
 import TextStats from "../components/TextStats";
 
@@ -8,6 +15,7 @@ import appConfig from "../config/appConfig";
 
 export default function Stats() {
   const [stats, setStats] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleFormSubmit = (formData) => {
     const url = `${appConfig.BASE_URL}/files/stats`;
@@ -16,33 +24,59 @@ export default function Stats() {
       .then((response) => {
         setStats(response?.data?.data);
       })
-      .catch((error) => {
-        console.error(error);
+      .catch((err) => {
+        console.log(err);
+        setError(err?.response?.data);
       });
   };
 
+  const handleClose = () => {
+    setError(null);
+  };
+
+  const alertAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={handleClose}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
   return (
-    <Container>
-      <Box pt={4}>
-        <Typography variant="h4" sx={{ mb: 3 }}>
-          Select a text file to check statistics.
-        </Typography>
-        <Box>
-          <StatsForm handleSubmit={handleFormSubmit} />
-          {stats ? (
-            <Box
-              p={2}
-              mt={2}
-              style={{ border: "1px solid #ddd", borderRadius: 4 }}
-            >
-              <Typography variant="h5" sx={{ mb: 3 }}>
-                File Statistics
-              </Typography>
-              <TextStats data={stats} />
-            </Box>
-          ) : null}
+    <Fragment>
+      <Container>
+        <Box pt={4}>
+          <Typography variant="h4" sx={{ mb: 3 }}>
+            Select a text file to check statistics.
+          </Typography>
+          <Box>
+            <StatsForm handleSubmit={handleFormSubmit} />
+            {stats ? (
+              <Box
+                p={2}
+                mt={2}
+                style={{ border: "1px solid #ddd", borderRadius: 4 }}
+              >
+                <Typography variant="h5" sx={{ mb: 3 }}>
+                  File Statistics
+                </Typography>
+                <TextStats data={stats} />
+              </Box>
+            ) : null}
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+      <Snackbar
+        open={error}
+        message={error?.message}
+        autoHideDuration={3000}
+        action={alertAction}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      />
+    </Fragment>
   );
 }
